b2dad0b64fedb3d5a391bbd85b9f9b83e6b38a56
pbivrell <prbivrell@gmail.com>
2017-10-01 22:31:31 -0600

    Update directory structure. Moved main to pol/main.go

--- src/lexer/lexer.go
+++ src/lexer/lexer.go
@@ -1,28 +1,35 @@
-package main
+package lexer
 
 import "fmt"
-import "io/ioutil"
 import "os"
 import "unicode"
 import "encoding/json"
 import "strings"
 
 /* Set by command line arguments. Enables non-error print statements */
-var debug = false
+var Debug = false
 
-/* lexer is a struct containing lexer information
+/* Lexer is a struct containing Lexer information
    - file is an array of runes read from a .pol file
    - pos is the index of the current rune
    - size is the size of file[]
    - lineno is the number of \n chars we have encountered
    - tokens is an array of tokens that is built as we tokenize
 */
-type lexer struct {
+type Lexer struct {
 	file      []rune
 	pos       int
 	size      int
 	lineno    int
-	hasErrors bool
+	HasErrors bool
+}
+
+/* Lexer constructor
+   creates and initalizes as Lexer
+*/
+
+func NewLexer(file string) *Lexer{
+	return &Lexer{[]rune(file),0,len(file),1,false}
 }
 
 /* token is a struct containing token information
@@ -35,14 +42,14 @@ type Token struct {
 	Value string `json:"value"`
 }
 
-/* Next is a method of the lexer struct. It does not
+/* Next is a method of the Lexer struct. It does not
    take any parameters and returns two types: a bool
    that represents whether or not we have reached
    the end of the file; and a rune that is the next
    utf-8 char in the file. After next has been called
    the position of the current rune is incremented
 */
-func (lex *lexer) next() (rune, bool) {
+func (lex *Lexer) next() (rune, bool) {
 	if lex.pos > lex.size-1 {
 		return 0, false
 	}
@@ -51,7 +58,7 @@ func (lex *lexer) next() (rune, bool) {
 	return lex.file[pos], true
 }
 
-/* Peek is a method of the lexer struct. It does not
+/* Peek is a method of the Lexer struct. It does not
    take any parameters and returns two types; a bool
    that represents whether or not we have reached
    the end of the file; and a rune that is the next
@@ -59,7 +66,7 @@ func (lex *lexer) next() (rune, bool) {
    the position of the current rune remains the same.
    (IE: subsequent calls to peek return the same results)
 */
-func (lex *lexer) peek() (rune, bool) {
+func (lex *Lexer) peek() (rune, bool) {
 	if lex.pos > lex.size-1 {
 		return 0, false
 	}
@@ -67,10 +74,10 @@ func (lex *lexer) peek() (rune, bool) {
 }
 
 /* Error is a function of the takes
-   three parameters a pointer to a lexer, an error message
+   three parameters a pointer to a Lexer, an error message
    and a token. Then it prints a syntax error.
 */
-func printError(lex *lexer, msg string, token string) {
+func printError(lex *Lexer, msg string, token string) {
 	token = strings.Replace(token,"\n","\\n",-1)
 	if len(token) > 10{
 		token = token[0:9]
@@ -79,7 +86,7 @@ func printError(lex *lexer, msg string, token string) {
 	fmt.Printf("Sytnax error: %s: Line %d: Token [%s]\n", msg,lex.lineno, token)
 	//Eat tokens while until whitespace or seperator
 	//Should decrease likelyhood of cascading errors
-	lex.hasErrors = true
+	lex.HasErrors = true
 	//This causes problems with the new line count
 	/*for char, hasNext := lex.next(); hasNext && !((isSeparator(char)) || unicode.IsSpace(char)); char, hasNext = lex.next() {
 		if char == '\n'{
@@ -90,17 +97,17 @@ func printError(lex *lexer, msg string, token string) {
 }
 
 /* Tokenize is a function that takes
-   a lexer pointer and iterates over the lexer taking identifying
+   a Lexer pointer and iterates over the Lexer taking identifying
    runes and calling appropriate function to tokenize the identified
    token.
 */
-func tokenize(lex *lexer) []Token {
+func Tokenize(lex *Lexer) []Token {
 	resTokens := []Token{}
 	for char, hasNext := lex.peek(); hasNext; char, hasNext = lex.peek() {
 		//New line
 		if char == '\n' {
 			// Printing when theres a new line feels like to much
-			/*if debug {
+			/*if Debug {
 				fmt.Println("New line")
 			}*/
 			lex.lineno = lex.lineno + 1
@@ -109,7 +116,7 @@ func tokenize(lex *lexer) []Token {
 			//Digit
 		} else if isDigit(char) {
 			tNum := GetNum(lex)
-			if debug {
+			if Debug {
 				fmt.Printf("Number: %+v\n", tNum)
 			}
 			resTokens = append(resTokens, tNum)
@@ -117,7 +124,7 @@ func tokenize(lex *lexer) []Token {
 			//String
 		} else if char == '"' {
 			tString := GetString(lex)
-			if debug {
+			if Debug {
 				fmt.Printf("String: %+v\n", tString)
 			}
 			resTokens = append(resTokens, tString)
@@ -125,7 +132,7 @@ func tokenize(lex *lexer) []Token {
 			//Indentifier
 		} else if (char >= 'a' && char <= 'z') || (char >= 'A' && char <= 'Z') {
 			tId := GetID(lex)
-			if debug {
+			if Debug {
 				fmt.Printf("ID: %+v\n", tId)
 			}
 			resTokens = append(resTokens, tId)
@@ -133,7 +140,7 @@ func tokenize(lex *lexer) []Token {
 			//Operators
 		} else if isOp := isOperator(char); isOp > 0 { //+ - * / ^ % || && = < > <= >= == != !
 			tOp := GetOp(lex)
-			if debug {
+			if Debug {
 				fmt.Printf("OP: %+v\n", tOp)
 			}
 			resTokens = append(resTokens, tOp)
@@ -141,7 +148,7 @@ func tokenize(lex *lexer) []Token {
 			//Seperators
 		} else if isSeparator(char) {
 			tSeparator := Token{"Separator", string(char)}
-			if debug {
+			if Debug {
 				fmt.Printf("Separator: %+v\n", tSeparator)
 			}
 			_, _ = lex.next()
@@ -149,7 +156,7 @@ func tokenize(lex *lexer) []Token {
 
 			//Should be the catch all for only whitespace
 		} else if unicode.IsSpace(char) {
-			if debug {
+			if Debug {
 				fmt.Println("Whitespace")
 			}
 			_, _ = lex.next()
@@ -162,12 +169,12 @@ func tokenize(lex *lexer) []Token {
 	return resTokens
 }
 
-/* getOp is a function that takes a lexer and
+/* getOp is a function that takes a Lexer and
    returns a new token. getOp assumes that you have
    already verified that the first rune is an operator
 */
 
-func GetOp(lex *lexer) Token {
+func GetOp(lex *Lexer) Token {
 	num, _ := lex.next()
 
 	//TODO rewrite this if statement it is ugly. Use some sort of Operator Map?
@@ -178,12 +185,12 @@ func GetOp(lex *lexer) Token {
 	return Token{"op", string(num)}
 }
 
-/* getNum is a function that takes a lexer pointer
+/* getNum is a function that takes a Lexer pointer
    and returns a token. getNum will eat runes by calling
-   lexer.next() until the result no longer matches [0-9]+(.[0-9]+)?
+   Lexer.next() until the result no longer matches [0-9]+(.[0-9]+)?
    getNum assumes that the first rune is a valid number
 */
-func GetNum(lex *lexer) Token {
+func GetNum(lex *Lexer) Token {
 	raw_res, _ := lex.next()
 	res := string(raw_res)
 
@@ -204,7 +211,7 @@ func GetNum(lex *lexer) Token {
 /* getDecimal is a function that will eat all of the runes
    that can be in a decimal point of a number
 */
-func getDecimal(lex *lexer) string {
+func getDecimal(lex *Lexer) string {
 	next, _ := lex.next()
 	res := string(next)
 	if num, hasNext := lex.peek(); !isDigit(num) || !hasNext{
@@ -227,13 +234,13 @@ func isDigit(char rune) bool {
 	return (char >= '0' && char <= '9')
 }
 
-/* getID is a function that takes a lexer pointer and returns
+/* getID is a function that takes a Lexer pointer and returns
    a token. getID eat runes calling lex.next() until it reaches an
    invalid identifier character ![0-9a-zA-Z]. getID assumes that
    the first rune is already a valid ID
 */
 
-func GetID(lex *lexer) Token {
+func GetID(lex *Lexer) Token {
 	raw_res, _ := lex.next()
 	res := string(raw_res)
 
@@ -249,13 +256,13 @@ func isID(char rune) bool {
 	return (char >= 'a' && char <= 'z') || (char >= 'A' && char <= 'Z') || (char >= '0' && char <= '9')
 }
 
-/* getString is a function that takes a lexer and returns a token.
+/* getString is a function that takes a Lexer and returns a token.
    getString will eat runes until it reaches a quote rune '"'. Note
    quotes can be escaped with a backslash allowing the rune '"' to
    be in a string. getString assumes that the first rune is a quote.
 */
 
-func GetString(lex *lexer) Token {
+func GetString(lex *Lexer) Token {
 	_, _ = lex.next()
 	res := ""
 	for char, hasNext := lex.peek(); hasNext; char, hasNext = lex.peek() {
@@ -352,7 +359,7 @@ func isSeparator(char rune) bool {
 /* Writes tokens to a json file. Will not write if some errors
    were generated during tokenization
 */
-func writeTokens(toks []Token, hasErrors bool, filename string) {
+func WriteTokens(toks []Token, hasErrors bool, filename string) {
 	if hasErrors {
 		fmt.Printf("Lexer: Failed to lex file %s see errors above!\n", filename)
 		return
@@ -377,26 +384,4 @@ func writeTokens(toks []Token, hasErrors bool, filename string) {
 	if write_err != nil {
 		panic(write_err)
 	}
-}
-
-func main() {
-	if len(os.Args) < 2 {
-		fmt.Printf("Usage: %s [.pol src file]\n", os.Args[0])
-		os.Exit(-1)
-	}
-
-	if len(os.Args) == 3 {
-		debug = true
-	}
-
-	data, err := ioutil.ReadFile(os.Args[1])
-	if err != nil {
-		panic(err)
-	}
-
-	lex := lexer{[]rune(string(data)), 0, len(data), 1, false}
-	pLex := &lex
-
-	tokens := tokenize(pLex)
-	writeTokens(tokens, pLex.hasErrors, os.Args[1])
 }
